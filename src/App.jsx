d1899eaa4674ace52942edd02e27627aa99f9449
suryaa6666 <suryaelidanto@gmail.com>
2023-03-27 11:28:47 +0700

    result

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,19 @@
+import Azmi, { Tyo } from "./components/Header";
 
-const nama = "muthee"
 
-function getNama(nama) {
-  return `nama saya ${nama}`
+const pencet = () => {
+  alert("halo")
 }
-
-function getBatch(nama) {
-  return `batch saya ${nama}`
-}
-
 
 function App() {
   return (
     <>
-      <Afra />
-      <Jeri />
-      <Alza></Alza>
-      <h1>{nama}</h1>
-      <h1>{getNama("alza")}</h1>
-      <h1>{getNama("afra")}</h1>
-      <h1>{getNama("robert")}</h1>
-      <h1>{getNama("gendi")}</h1>
-      <h1>{getNama("teguh")}</h1>
-      <h1>{getNama("tyo")}</h1>
-      <h1>{getBatch("45")}</h1>
+      <button onClick={pencet}>click saya</button>
+      <button onClick={pencet}>click saya</button>
+      <Azmi />
+      <Tyo />
     </>
   );
-}
-
-function Afra() {
-  return (
-    <h1>Hai aku asdsad</h1>
-  )
-}
-
-function Jeri() {
-  return (
-    <h1>Hai aku Jeri</h1>
-  )
-}
-
-function Alza() {
-  return (
-    <h1>Hai aku Alza</h1>
-  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Surya() {
+    return (
+        <h1>Ini adalah test</h1>
+    )
+}
+
+export function Tyo() {
+    return (
+        <h1>Ini adalah tyo</h1>
+    )
+}
