b896beb33253fa0a7561118fca9bd11b3e7c6155
suryaa6666 <suryaelidanto@gmail.com>
2023-03-27 10:32:02 +0700

    result

---
+++ src/App.js
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <>
+      <Afra />
+      <Jeri />
+      <Alza></Alza>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+function Afra() {
+  return (
+    <h1>Hai aku asdsad</h1>
+  )
+}
+
+function Jeri() {
+  return (
+    <h1>Hai aku Jeri</h1>
+  )
+}
+
+function Alza() {
+  return (
+    <h1>Hai aku Alza</h1>
+  )
+}
+
+export default App;
